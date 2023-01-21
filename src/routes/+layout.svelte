65fcc8a1af8d8951ba5e867c14bd0c8dbb728f0d
Creaperhunter <creaperhunter531@gmail.com>
2023-01-21 21:18:00 +0100

    Hehe

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,37 @@
 <script>
-	import './styles.css';
+	// import './styles.css';
+	import seizure from '$lib/images/seizure.png';
 </script>
 
 <main>
-	<slot />
+	<div class="fimage">
+		<img class="fitted" alt="siezure" src={seizure} />
+	</div>
+	<!-- <slot /> -->
 </main>
 
-<footer>Made By Creaperhunter#0001</footer>
-
+<!-- <footer>Made By Creaperhunter#0001</footer> -->
 <style>
-	footer {
+	* {
+		margin: 0;
+		padding: 0;
+	}
+	.fimage {
+		display: grid;
+		height: 100%;
+		justify-content: center;
+	}
+	.fitted {
+		max-width: 100%;
+		max-height: 100vh;
+		margin: auto;
+		border: none;
+	}
+	:global(body) {
+		overflow: hidden;
+		background-color: #000000;
+	}
+	/* footer {
 		text-align: center;
 		width: 100%;
 		position: fixed;
@@ -23,5 +45,5 @@
 		flex-direction: column;
 		justify-content: center;
 		min-height: 100vh;
-	}
+	} */
 </style>
